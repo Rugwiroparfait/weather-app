d4dc7cc4362a144e2b6a3207e187727412c65c67
Rugwiroparfait <rugwiroparfait003@gmail.com>
2025-05-19 23:26:42 +0200

    initialized the app

---
+++ backend/app.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('MongoDB connection error:', err));
+
+// Simple test route
+app.get('/', (req, res) => {
+  res.send('Weather API running');
+});
+
+// TODO: Add routes here
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
